6ebc105ea674969aca1d638dad393d51cb9f8578
theculliganman <rrculligan@gmail.com>
2025-06-29 13:37:30 -0500

    chore: remove TypeScript ESLint configuration and adjust rules for unused variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,19 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
-    ecmaFeatures: {
-      jsx: true,
-    },
   },
   extends: [
     'eslint:recommended',
-    '@typescript-eslint/recommended',
   ],
-  plugins: ['@typescript-eslint'],
   env: {
     browser: true,
     es6: true,
     node: true,
   },
   rules: {
-    'no-unused-vars': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+    'no-unused-vars': 'warn',
     'no-console': 'warn',
   },
 };
