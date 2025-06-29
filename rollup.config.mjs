a48828b6c03608caef6c7b8d4213b2db10051c9e
theculliganman <rrculligan@gmail.com>
2025-06-28 21:23:53 -0500

    fix: update package.json import assertion syntax in Rollup configuration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
-import packageJson from './package.json' assert { type: 'json' };
+import packageJson from './package.json' with { type: 'json' };
 
 export default {
   input: 'src/index.ts',
